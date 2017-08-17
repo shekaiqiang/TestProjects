83570b6ea4b7242d370be17d2d2cddbbdb4e6534
iTanken <619552334@qq.com>
2017-08-17 17:51:09 +0800

    Test

--- zTest/src/com/JCom.java
+++ zTest/src/com/JCom.java
@@ -5,31 +5,33 @@ import jp.ne.so_net.ga2.no_ji.jcom.ReleaseManager;
 
 public class JCom {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		JCom jc = new JCom();
-		jc.icDecode("");
-	}
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        JCom jc = new JCom();
+        jc.icDecode("");
+    }
 
+    public String icDecode(String cICID) throws Exception {
+        ReleaseManager rm = new ReleaseManager();
+        IDispatch vbcom = new IDispatch(rm, "AHisYLK.JK"); // YYTYBJK.YBJK
+        System.out.println(vbcom.toString());
 
-	  public String icDecode(String cICID) throws Exception {
-	    ReleaseManager rm = new ReleaseManager();
-	    IDispatch vbcom = new IDispatch(rm, "AHisYLK.JK");
+        Object[] param = { "", "", cICID, "" };
+        vbcom.method("checked", param);
+        String errInfo = (String) vbcom.get("LastError");
+        if (!isEmptyStr(errInfo)) {
+            throw new Exception(errInfo);
+        }
+        cICID = (String) vbcom.get("Rstr");
+        
+        vbcom.release();
+        return cICID;
+    }
 
-	    Object[] param = { "", "", cICID, "" };
-	    vbcom.method("checked", param);
-	    String errInfo = (String)vbcom.get("LastError");
-	    if (!IsEmptyStr(errInfo)) {
-	      throw new Exception(errInfo);
-	    }
-	    cICID = (String)vbcom.get("Rstr");
-	    vbcom.release();
-	    return cICID;
-	  }
-		public static boolean IsEmptyStr(String value) {
-			return value==null||"".equals(value)||"".equals(value.trim())||"null".equalsIgnoreCase(value);
-		}
+    public static boolean isEmptyStr(String value) {
+        return value == null || "".equals(value) || "".equals(value.trim()) || "null".equalsIgnoreCase(value);
+    }
 }
