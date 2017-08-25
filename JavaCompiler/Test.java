6656f456410a986ccf41b1034265ce9dcc46ef51
iTanken <619552334@qq.com>
2017-08-25 11:43:57 +0800

    Test

--- JavaCompiler/Test.java
+++ JavaCompiler/Test.java
@@ -1,5 +1,5 @@
 public class Test{
   public static void main(String[] args) {
-    System.out.println("Hello World!!!这是一个测试程序！！！");
+    System.out.println("Hello World!!!");
   }
 }
---
+++ zTest/src/com/COMBusy.java
@@ -0,0 +1,60 @@
+package com;
+
+import jp.ne.so_net.ga2.no_ji.jcom.IDispatch;
+import jp.ne.so_net.ga2.no_ji.jcom.JComException;
+import jp.ne.so_net.ga2.no_ji.jcom.ReleaseManager;
+
+public class COMBusy {
+
+    private static IDispatch inface = null; // COM接口
+
+    public COMBusy(String progID) throws Exception {
+        if (inface == null) {
+            try {
+                ReleaseManager rm = new ReleaseManager();
+                inface = new IDispatch(rm, progID);
+            } catch (JComException e) {
+                inface = null;
+                throw new Exception("调用 COM 接口：COM 接口初始化失败！");
+            }
+            System.out.println("调用 COM 接口：COM 初始化成功：" + IDispatch.IID);
+        }
+    }
+
+    public Object callHisCom(String method, String inEle) throws Exception {
+        String reqStr = "<?xml version=\"1.0\" encoding=\"GBK\"?>" + inEle;
+        Object[] param = new Object[] { "XXX", reqStr };
+        try {
+            String OutValue = inface.method(method, param).toString();
+            if ("0".equals(OutValue.substring(0, 1))) {
+                System.out.println("调用 COM 接口：执行（" + method + "）失败！" + OutValue.substring(1, OutValue.length()));
+            }
+            return OutValue;
+        } catch (JComException e) {
+            e.printStackTrace();
+            return "调用COM接口：执行Execute（"+ method +"）失败！";
+        } finally {
+            inface.release();
+        }
+    }
+
+    /**
+     * Test 
+     * @param args inArgs
+     */
+    public static void main(String[] args) {
+        COMBusy com = null;
+        Object obj = null;
+        try {
+            com = new COMBusy("YYTYBJK.YBJK");
+            obj = com.callHisCom("Execute", "<Invalue><ZDBH>ZDBH01</ZDBH></invalue>");
+            System.out.println(obj);
+            
+            // com = new COMBusy("YxPhone.Phone");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            com = null;
+        }
+    }
+}
